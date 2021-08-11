15bffe6159777b25523753b0767c95463c3194fe
Peter Abdelshahid <peter.essam@gmail.com>
2021-08-11 14:26:11 +0200

    Search Page and progress bar

---
+++ pages/search.js
@@ -0,0 +1,71 @@
+import { useRouter } from "next/dist/client/router";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
+
+function Search({ searchResults }) {
+  const router = useRouter();
+
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} to ${formattedEndDate}`;
+
+  return (
+    <div className="">
+      <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
+
+      <main className="flex">
+        <section className="flex-grow pt-14 px-16">
+          <p className="text-xs">
+            300+ Stays from {range} for {noOfGuests} Guests
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6 ">
+            Stays in {location}
+          </h1>
+
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap ">
+            <p className="mod-button">Cancellation Flexibility</p>
+            <p className="mod-button">Type of Place Flexibility</p>
+            <p className="mod-button">Price</p>
+            <p className="mod-button">Rooms</p>
+            <p className="mod-button">More filters</p>
+          </div>
+          <div className="flex flex-col">
+            {searchResults?.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
